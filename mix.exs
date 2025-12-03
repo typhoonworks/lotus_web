68c5cd3b8181a29b63a4cacb21edbadf5cf905ad
Arda Tugay <ardactugay@gmail.com>
2025-12-03 11:21:12 +0800

    Downgrade Gettext to ~> 0.26

--- mix.exs
+++ mix.exs
@@ -44,7 +44,7 @@ defmodule Lotus.Web.MixProject do
       {:phoenix_live_view, ">= 1.0.0 and < 1.2.0"},
       {:phoenix_html, "~> 4.0"},
       {:phoenix_ecto, "~> 4.5"},
-      {:gettext, "~> 1.0"},
+      {:gettext, "~> 0.26"},
 
       # Lotus
       {:lotus, "~> 0.9.2"},
