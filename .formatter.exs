3a6efffd3316f951fb7f82867ec996f8fb0533e2
Rui Freitas <rodloboz@hey.com>
2026-03-05 14:02:54 +0000

    Add locals_without_parens export for lotus_dashboard macro
    Prevents mix format from adding parentheses to lotus_dashboard
    calls in consumer projects.
    
    Closes #66

--- .formatter.exs
+++ .formatter.exs
@@ -1,4 +1,8 @@
 # Used by "mix format"
+locals_without_parens = [lotus_dashboard: 1, lotus_dashboard: 2]
+
 [
-  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
+  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
+  locals_without_parens: locals_without_parens,
+  export: [locals_without_parens: locals_without_parens]
 ]
