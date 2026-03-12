e664f0c67d35a98363e2a94cd0edddf4fc12809a
Russell Matbouli <russell.matbouli@gmail.com>
2026-03-12 14:53:41 +0000

    Replace height calculation with full height

--- lib/lotus/web/pages/query_editor_page.ex
+++ lib/lotus/web/pages/query_editor_page.ex
@@ -118,7 +118,7 @@ defmodule Lotus.Web.QueryEditorPage do
                 />
               </div>
 
-              <div class="flex-1 min-h-0">
+              <div class="flex-1 min-h-1">
                 <.render_result
                   query_id={Map.get(@page, :id, "new")}
                   result={@result}
